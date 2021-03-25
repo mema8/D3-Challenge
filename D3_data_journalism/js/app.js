babea197d6bc6abd64b5a395c8751eec10ca90c9
mema8 <mansoor.mays@gmail.com>
2021-03-25 15:40:14 -0700

    2

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,15 +1,15 @@
 // @TODO: YOUR CODE HERE!
 function makeResponsive() {
 
-    // Reset SVG area when browser load and replace with resixed chart
+   
     var svgArea = d3.select('body').select('svg');
 
-    // Clear svg
+   
     if (!svgArea.empty()) {
         svgArea.remove();
     }
 
-    // SVG dimentoins as the current window 
+    
     var svgWidth = window.innerWidth; 
     var svgHeight = window.innerHeight; 
     
@@ -26,43 +26,43 @@ function makeResponsive() {
     var width = svgWidth - margin.left - margin.right; 
     var height = svgHeight - margin.top - margin.bottom; 
 
-    // Append SVG element
+   
     var svg = d3.select('#scatter')
         .append('svg')
         .attr('height', svgHeight)
-        // .attr('class', 'chart')
+
+        
         .attr('width', svgWidth);
 
-    // Append group element 
+     
     var chartGroup = svg.append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    // Read CSV file 
+     
     d3.csv('./D3_data_journalism/data/data.csv').then(function(dataSmokers) {
 
-        // Parse data 
+         
         dataSmokers.forEach(function(data) {
             data.smokes =+data.smokes; 
             data.age =+data.age; 
-            // console.log("smokes: ", d.smokes);
-            // console.log("ages: " , d.age)
+            
         }); 
 
-        // Scales 
+        
         var xBandScale = d3.scaleLinear()
             .domain([20, d3.extent(data, d => d.smokes)])
             .range([0, width]);
-            // .padding(0.1);
+          
         
         var yLinearScale = d3.scaleLinear()
             .domain([0, d3.extent(data, d => d.age)])
             .range([height, 0]);
         
-        // Create axis 
+         
         var bottomAxis = d3.axisBottom(xBandScale);
         var leftAxis = d3.axisLeft(yLinearScale);
     
-        // Append axis 
+         
         chartGroup.append('g')
             .attr('transform', `translate(0, ${height})`)
             .call(bottomAxis);
@@ -70,7 +70,7 @@ function makeResponsive() {
         chartGroup.append('g')
             .call(leftAxis);
     
-        // Append circles 
+         
         var circlesGroup = chartGroup.selectAll('circle')
             .data(dataSmokers)
             .enter()
@@ -80,39 +80,37 @@ function makeResponsive() {
             .attr('r', '10')
             .attr('fill', 'gold')
             .attr('opacity', '.5');
-            // .attr('stroke', 'yellow')
-            // .attr('stroke-width', '1');
+            
 
-        // Create tool tip 
-        // Initialize Tooltip
+        
         var toolTip = d3.tip()
         .select('body')
         .append('toolTip')
         .attr("class", "d3-tip")
         
-        // positioning the blue box (the info box)
+        
         .offset([80, -60])
         .html(function(d) {
             return (`<strong>${d.smokes}<strong><hr>${d.age}`);
       });
 
-        // Create the tooltip in chartGroup.
+        
         chartGroup.call(toolTip);
 
-        // Create "mouseover" event listener to display tooltip
+        
         circlesGroup.on("mouseover", function(data, i) {
         toolTip.style('display', 'block');
         toolTip.html(`${data.abbr}`);
         })
         
         
-        // Create "mouseout" event listener to hide tooltip
+        
         .on("mouseout", function() {
             toolTip.style('display', 'none');
         });
 
 
-        // Create axes labels
+        
         chartGroup.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 0 - margin.left + 40)
@@ -133,8 +131,8 @@ function makeResponsive() {
     });
 }
 
-// When the browser loads, makeResponsive() is called
+
 makeResponsive();
 
-// When the browser window is resized, makeResponsive() is called.
+
 d3.select(window).on("resize", makeResponsive);
